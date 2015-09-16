294238429890c687a91c970973c90137f4fb93d3
Alex Corkin <alex.corkin@sparkventures.co.nz>
2015-09-16 14:01:39 +1200

    autoformat commit

--- src/main/java/communityblogger/services/BloggerResolver.java
+++ src/main/java/communityblogger/services/BloggerResolver.java
@@ -11,38 +11,38 @@ import javax.xml.bind.JAXBException;
 
 @Provider
 @Produces("application/xml")
-public class BloggerResolver implements ContextResolver<JAXBContext>{
-	private JAXBContext _context;
-	
-	// TO DO:
-	// Add to the _classes array the classes whose object are to be marshalled/
-	// unmarshalled to/from XML. Objects of these class will be exchanged by
-	// clients and your Web service.
-	private Class<?>[] _classes = {User.class};
+public class BloggerResolver implements ContextResolver<JAXBContext> {
+    private JAXBContext _context;
+
+    // TO DO:
+    // Add to the _classes array the classes whose object are to be marshalled/
+    // unmarshalled to/from XML. Objects of these class will be exchanged by
+    // clients and your Web service.
+    private Class<?>[] _classes = {User.class};
 
 //	Marshaller marshaller = _context.createMarshaller();
 //	Unmarshaller unmarshaller = _context.createUnmarshaller();
-	
-	public BloggerResolver() {
-		try {
 
-			_context = JAXBContext.newInstance(_classes);
-			
-			// Initialise the context as required.
-		} catch(JAXBException e) {
-			e.printStackTrace();
-		}
-	}
+    public BloggerResolver() {
+        try {
 
-	@Override
-	public JAXBContext getContext(Class<?> type) {
+            _context = JAXBContext.newInstance(_classes);
+
+            // Initialise the context as required.
+        } catch (JAXBException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public JAXBContext getContext(Class<?> type) {
 
 
-		for(int i = 0; i < _classes.length; i++) {
-			if(type.equals(_classes[i])) {
-				return _context;
-			}
-		}
-		return null;
-	}
+        for (int i = 0; i < _classes.length; i++) {
+            if (type.equals(_classes[i])) {
+                return _context;
+            }
+        }
+        return null;
+    }
 }
--- src/main/java/communityblogger/services/BloggerResource.java
+++ src/main/java/communityblogger/services/BloggerResource.java
@@ -1,80 +1,74 @@
 package communityblogger.services;
 
-import java.util.Set;
-
 import javax.ws.rs.*;
-import javax.ws.rs.container.AsyncResponse;
-import javax.ws.rs.container.Suspended;
-import javax.ws.rs.core.Cookie;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("resources")
 public interface BloggerResource {
 
-	// TO DO:
-	// Define one method for each of the operations in the Community Blogger
-	// Web service contract. In each case, use appropriate JAX-RS annotations
-	// to specify the URI pattern, media  type and HTTP method.
-	//
-	// The service contract comprises the 8 operations:
-	// - Create user
-	// - Retrieve user
-	// - Create blog entry
-	// - Retrieve blog entry
-	// - Create comment
-	// - Retrieve comments
-	// - Retrieve blog entries
-	// - Follow blog entry
+    // TO DO:
+    // Define one method for each of the operations in the Community Blogger
+    // Web service contract. In each case, use appropriate JAX-RS annotations
+    // to specify the URI pattern, media  type and HTTP method.
+    //
+    // The service contract comprises the 8 operations:
+    // - Create user
+    // - Retrieve user
+    // - Create blog entry
+    // - Retrieve blog entry
+    // - Create comment
+    // - Retrieve comments
+    // - Retrieve blog entries
+    // - Follow blog entry
 
-	
-	/**
-	 * Useful operation to initialise the state of the Web service. This operation 
-	 * can be invoked prior to executing each unit test.
-	 */
-	@PUT
-	@Produces("application/xml")
-	void initialiseContent();
 
-	@Path("/createUser/{username}/{lastname}/{firstname}")
-	@POST
-	@Produces("application/xml")
-	Response createUser(@PathParam("username") String username,
-						@PathParam("lastname") String lastname,
-						@PathParam("firstname") String firstname);
+    /**
+     * Useful operation to initialise the state of the Web service. This operation
+     * can be invoked prior to executing each unit test.
+     */
+    @PUT
+    @Produces("application/xml")
+    void initialiseContent();
 
-	@Path("/retrieveUser/{username}")
-	@GET
-	@Produces("application/xml")
-	Response retrieveUser(@PathParam("username") String username);
+    @Path("/createUser/{username}/{lastname}/{firstname}")
+    @POST
+    @Produces("application/xml")
+    Response createUser(@PathParam("username") String username,
+                        @PathParam("lastname") String lastname,
+                        @PathParam("firstname") String firstname);
 
-	@Path("/createBlogEntry")
-	@POST
-	@Produces("application/xml")
-	void createBlogEntry();
+    @Path("/retrieveUser/{username}")
+    @GET
+    @Produces("application/xml")
+    Response retrieveUser(@PathParam("username") String username);
 
-	@Path("/retrieveBlogEntry")
-	@GET
-	@Produces("application/xml")
-	void retrieveBlogEntry();
+    @Path("/createBlogEntry")
+    @POST
+    @Produces("application/xml")
+    void createBlogEntry();
 
-	@Path("/createComment")
-	@POST
-	@Produces("application/xml")
-	void createComment();
+    @Path("/retrieveBlogEntry")
+    @GET
+    @Produces("application/xml")
+    void retrieveBlogEntry();
 
-	@Path("/retrieveComments")
-	@GET
-	@Produces("application/xml")
-	void retrieveComments();
+    @Path("/createComment")
+    @POST
+    @Produces("application/xml")
+    void createComment();
 
-	@Path("/retrieveBlogEntries")
-	@POST
-	@Produces("application/xml")
-	void retrieveBlogEntries();
+    @Path("/retrieveComments")
+    @GET
+    @Produces("application/xml")
+    void retrieveComments();
 
-	@Path("/followBlogEntry")
-	@PUT
-	@Produces("application/xml")
-	void followBlogEntry();
+    @Path("/retrieveBlogEntries")
+    @POST
+    @Produces("application/xml")
+    void retrieveBlogEntries();
+
+    @Path("/followBlogEntry")
+    @PUT
+    @Produces("application/xml")
+    void followBlogEntry();
 }
